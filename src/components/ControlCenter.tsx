f73181d12e43b4dfd93605004166400056d7806b
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2025-02-07 11:19:59 -0800

    updated

---
+++ src/components/ControlCenter.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import TrackPlayer from 'react-native-track-player'
+const ControlCenter = () => {
+  return (
+    <View>
+      <Text>ControlCenter</Text>
+    </View>
+  )
+}
+
+export default ControlCenter
+
+const styles = StyleSheet.create({})
