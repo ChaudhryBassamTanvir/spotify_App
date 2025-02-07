3af1080ff59f10569bb76b833117318d35b11b34
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2025-02-07 08:14:49 -0800

    updated

---
+++ src/constants.ts
@@ -0,0 +1,33 @@
+import { Track } from "react-native-track-player";
+
+export const playListData:Track[]=[
+    {
+    id:1,
+    title:"8 Parche",
+    artist:"Baani Sandhu",
+    album:"Jaani",
+    artwork:"1.jpg",
+    url:require('./assets/8_Parche.mp3')
+},{
+    id:2,
+    title:"Shakar Wandare",
+    artist:"Hoo Maan jaha",
+    album:"",
+    artwork:"2.jpg",
+    url:require('./assets/Shakar_Wandanre.mp3')
+},{
+    id:3,
+    title:"Meri Tanhai",
+    artist:"Kubra Khan",
+    album:"O Piya re",
+    artwork:"3.jpg",
+    url:require('./assets/Meri_Tanhai.mp3')
+},{
+    id:4,
+    title:"Meri Tanhai",
+    artist:"Kubra Khan",
+    album:"O Piya re",
+    artwork:"3.jpg",
+    url:require('./assets/Ask_mi_lazim.mp3')
+}
+]
