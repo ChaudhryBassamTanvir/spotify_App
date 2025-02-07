1d482e9f3088a6a0e9ac591d9b924153d2b299f9
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2025-02-07 10:49:13 -0800

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,20 @@ import React,{useState,useEffect } from 'react'
 
 import { setupPlayer,addTrack } from '../musicPlayerServices'
 const App = () => {
+
+
+
+  const [isPlayerReady,setIsPlayerReady]=useState(false)
+
+  async function setup() {
+    let isSetup= await setupPlayer()
+  }
+
+useEffect(()=>{
+
+
+},[])
+
   return (
     <View>
       <Text>Spotify like clone</Text>
