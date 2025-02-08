2cc584c02db4003f47c64ba7f24f2ef824edf44b
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2025-02-08 08:53:27 -0800

    song info

---
+++ src/components/Songinfo.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Songinfo = () => {
+  return (
+    <View>
+      <Text>Songinfo</Text>
+    </View>
+  )
+}
+
+export default Songinfo
+
+const styles = StyleSheet.create({})
